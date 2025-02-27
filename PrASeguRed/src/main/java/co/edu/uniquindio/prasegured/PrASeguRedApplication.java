93b32d038743120beb073ebcfaea67662385acf9
Luis Carlos Calderón Calvo <128423397+Luisosky@users.noreply.github.com>
2025-02-27 09:15:11 -0500

    Creación Proyecto

---
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/PrASeguRedApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.prasegured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrASeguRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrASeguRedApplication.class, args);
+    }
+
+}
---
+++ PrASeguRed/src/test/java/co/edu/uniquindio/prasegured/PrASeguRedApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.prasegured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrASeguRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
