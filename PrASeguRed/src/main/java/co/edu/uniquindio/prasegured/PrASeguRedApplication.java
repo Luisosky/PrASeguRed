c13fb6ac1ad1ca8f08e55a68e96efc8805792fa1
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-17 09:44:49 -0500

    Correcion openapi

--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/PrASeguRedApplication.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/PrASeguRedApplication.java
@@ -2,7 +2,7 @@ package co.edu.uniquindio.prasegured;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+    
 @SpringBootApplication
 public class PrASeguRedApplication {
 
