73d4902b3fc40d3e8c54dd54af2347932c43925c
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-22 20:59:20 -0500

    controladores e implementacion de captcha

---
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/security/RecaptchaService.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.prasegured.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.Map;
+
+@Service
+public class RecaptchaService {
+
+    @Value("${google.recaptcha.secret}")
+    private String recaptchaSecret; // 🔑 Clave secreta
+
+    private static final String RECAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify";
+
+    public boolean verifyRecaptcha(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = RECAPTCHA_URL + "?secret=" + recaptchaSecret + "&response=" + token;
+
+        Map<String, Object> response = restTemplate.postForObject(url, null, Map.class);
+
+        if (response == null || !response.containsKey("success")) {
+            return false;
+        }
+
+        return (boolean) response.get("success");
+    }
+}
