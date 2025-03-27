0c94bb4650fcc99ec9c3ddd5582b17b29d49a794
Luisosky <luisc.calderonc@uqvirtual.edu.co>
2025-03-27 16:49:08 -0500

    feat: Se implementa la logica de log in para el endpoint del front

--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/dto/CredencialesDTO.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/dto/CredencialesDTO.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.prasegured.dto;
 
 public record CredencialesDTO(String correo, String contraseña) {
+    @Override
+    public String correo() {
+        return correo;
+    }
+
+    @Override
+    public String contraseña() {
+        return contraseña;
+    }
 }
