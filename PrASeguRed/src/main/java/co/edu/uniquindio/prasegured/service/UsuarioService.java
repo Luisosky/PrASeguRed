8192e34c4558e9c81a2b4fd1995ce95e1d241437
Luisosky <luisc.calderonc@uqvirtual.edu.co>
2025-03-27 10:01:17 -0500

    feat: Implementación de contraseña encriptada

--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/UsuarioService.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/UsuarioService.java
@@ -3,14 +3,19 @@ package co.edu.uniquindio.prasegured.service;
 import co.edu.uniquindio.prasegured.model.Usuario;
 import co.edu.uniquindio.prasegured.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public Usuario registrarUsuario(Usuario usuario){
+        String rawPassword = usuario.getDocumento() + usuario.getContraseña();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        usuario.setContraseña(encodedPassword);
         return usuarioRepository.save(usuario);
     }
 }
