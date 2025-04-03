a4df4fdd6c003aa4a13f4b60d456fed1c6b42289
Casb29XD <80225020+Casb29XD@users.noreply.github.com>
2025-04-03 16:12:06 -0500

    lol

--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/ReporteService.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/ReporteService.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.prasegured.service;
 
+import co.edu.uniquindio.prasegured.dto.ReporteDTO;
+import co.edu.uniquindio.prasegured.dto.ReporteRequest;
 import co.edu.uniquindio.prasegured.model.Reporte;
 import co.edu.uniquindio.prasegured.repository.ReporteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +10,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ReporteService {
-
-    @Autowired
-    private ReporteRepository reporteRepository;
-
-    public Reporte registrarReporte(Reporte reporte) {
-        return reporteRepository.save(reporte);
-    }
-
-    public Reporte actualizarReporte(String id, Reporte reporte) {
-        // Implementa la lógica para actualizar un reporte
-    }
-
-    public List<Reporte> getAllReportes() {
-        return reporteRepository.findAll();
-    }
-
-    public void eliminarReporte(String id) {
-        // Implementa la lógica para eliminar un reporte
-    }
+public interface ReporteService {
+    ReporteDTO save(ReporteRequest reporte);
+    ReporteDTO update(String id, Reporte reporte);
+    List<ReporteDTO> findAll();
+    ReporteDTO findById(String id);
+    void deleteById(String id);
 }
--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/ReporteServiceImple.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/service/ReporteServiceImple.java
@@ -1,4 +1,20 @@
 package co.edu.uniquindio.prasegured.service;
 
-public interface ReporteServiceImple {
+import co.edu.uniquindio.prasegured.dto.ReporteDTO;
+import co.edu.uniquindio.prasegured.dto.ReporteRequest;
+import co.edu.uniquindio.prasegured.mapper.ReporteMapper;
+import co.edu.uniquindio.prasegured.repository.ReporteRepository;
+
+public class ReporteServiceImple implements ReporteService {
+    private final ReporteRepository reporteRepository;
+    private final ReporteMapper reporteMapper;
+
+    @Override
+    public ReporteDTO save(ReporteRequest reporte) {
+        var newReporte = reporteMapper.parseOf(reporte);
+        validateReporteName(reporte.id());
+        return reporteMapper.toReporteDTO(
+                reporteRepository.save(newReporte)
+        );
+    }
 }
