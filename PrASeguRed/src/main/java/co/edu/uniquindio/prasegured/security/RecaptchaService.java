10828ea91e2882e060628407d7fac045acd4953f
Daniel <88367009+William19D@users.noreply.github.com>
2025-04-10 08:16:24 -0500

    quitando comentario que WTF

--- PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/security/RecaptchaService.java
+++ PrASeguRed/src/main/java/co/edu/uniquindio/prasegured/security/RecaptchaService.java
@@ -7,25 +7,25 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-//@Configuration
-//@Profile("!test")
-//@Service
+@Configuration
+@Profile("!test")
+@Service
 public class RecaptchaService {
 
-//    private final String recaptchaSecret;
-//    private static final String RECAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify";
-//
-//    public RecaptchaService(String recaptchaSecret) {
-//        this.recaptchaSecret = recaptchaSecret;
-//    }
-//
-//    public boolean verifyRecaptcha(String token) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        String url = RECAPTCHA_URL + "?secret=" + recaptchaSecret + "&response=" + token;
-//
-//        Map<String, Object> response = restTemplate.postForObject(url, null, Map.class);
-//
-//        return response != null && Boolean.TRUE.equals(response.get("success"));
-//    }
+    private final String recaptchaSecret;
+    private static final String RECAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify";
+
+    public RecaptchaService(String recaptchaSecret) {
+        this.recaptchaSecret = recaptchaSecret;
+    }
+
+    public boolean verifyRecaptcha(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = RECAPTCHA_URL + "?secret=" + recaptchaSecret + "&response=" + token;
+
+        Map<String, Object> response = restTemplate.postForObject(url, null, Map.class);
+
+        return response != null && Boolean.TRUE.equals(response.get("success"));
+    }
 }
 
